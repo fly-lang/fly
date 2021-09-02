f94f0c1d3b6192f349751351ddb430d1e5496fe3
Fly Lang <dev@flylang.org>
2021-09-02 18:59:15 +0200

    Update Google Tests

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Support/Signals.h"
+#include "llvm/ADT/StringRef.h"
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
