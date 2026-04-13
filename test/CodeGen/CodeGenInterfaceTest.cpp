5d036938ae1d254a3295a32803da06b893176ff6
fly-lang <dev@flylang.org>
2026-04-13 19:44:27 +0200

    Refactor error handling in CodeGen: update error type structure and improve object storage logic

--- test/CodeGen/CodeGenInterfaceTest.cpp
+++ test/CodeGen/CodeGenInterfaceTest.cpp
@@ -43,17 +43,21 @@ namespace {
     	llvm::SmallVector<ASTType *, 4> BaseInterfaces;
     	ASTClass *BaseInterface = ASTBuilder::CreateClass(Module, SourceLoc, ASTClassKind::INTERFACE,
     		"BaseInterface", TopModifiers, BaseInterfaces);
-    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface, "do", TopModifiers, Params);
+    	ASTBlockStmt *DoBody = ASTBuilder::CreateBlockStmt(SourceLoc);
+    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface, "do", TopModifiers, Params, DoBody);
 
     	// Generate Code
     	Generate();
 		llvm::Module * M = getModules()[0];
     	std::string output = getOutput(M);
 
-    	EXPECT_EQ(output, "\n");
+    	EXPECT_EQ(output, "\n%error = type { i32, ptr, ptr }\n"
+    	                  "\n"
+    	                  "@error = external constant %error\n"
+    	                  "@vtable.BaseInterface = constant [1 x ptr] zeroinitializer\n");
     }
 
-	TEST_F(CodeGenTest, DISABLED_CGInterfaceExtendsInterfaces) {
+	TEST_F(CodeGenTest, CGInterfaceExtendsInterfaces) {
         /**
          * Fly code:
          * interface BaseInterface {
@@ -63,8 +67,11 @@ namespace {
          *   undo()
          * }
          * interface TestInterface : BaseInterface, BaseInterface2 {
-         *   call()
+         *
          * }
+         * TestClass : TestInterface {
+		 *   do() {}
+		 *   undo() {}
          */
         ASTModule *Module = CreateModule();
 
@@ -76,7 +83,8 @@ namespace {
     	llvm::SmallVector<ASTType *, 4> BaseInterfaces;
     	ASTClass *BaseInterface = ASTBuilder::CreateClass(Module, SourceLoc, ASTClassKind::INTERFACE,
     		"BaseInterface", TopModifiers, BaseInterfaces);
-    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface, "do", TopModifiers, Params);
+    	ASTBlockStmt *IDoBody = ASTBuilder::CreateBlockStmt(SourceLoc);
+    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface, "do", TopModifiers, Params, IDoBody);
 
 		// interface BaseInterface2 {
 		//   undo()
@@ -86,7 +94,8 @@ namespace {
     	llvm::SmallVector<ASTType *, 4> Base2Interfaces;
     	ASTClass *BaseInterface2 = ASTBuilder::CreateClass(Module, SourceLoc, ASTClassKind::INTERFACE,
     		"BaseInterface2", TopModifiers, Base2Interfaces);
-    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface2, "undo", TopModifiers, Params);
+    	ASTBlockStmt *IUndoBody = ASTBuilder::CreateBlockStmt(SourceLoc);
+    	ASTBuilder::CreateClassMethod(SourceLoc, BaseInterface2, "undo", TopModifiers, Params, IUndoBody);
 
 		//
 		// interface TestInterface : BaseInterface, BaseInterface2 {}
@@ -117,7 +126,58 @@ namespace {
 		llvm::Module * M = getModules()[0];
     	std::string output = getOutput(M);
 
-    	EXPECT_EQ(output, "\n");
+    	EXPECT_EQ(output, "\n%error = type { i32, ptr, ptr }\n"
+    	                  "%TestClass = type { ptr, %TestInterface }\n"
+    	                  "%TestInterface = type { ptr, %BaseInterface, %BaseInterface2 }\n"
+    	                  "%BaseInterface = type { ptr }\n"
+    	                  "%BaseInterface2 = type { ptr }\n"
+    	                  "\n"
+    	                  "@error = external constant %error\n"
+    	                  "@vtable.BaseInterface = constant [1 x ptr] zeroinitializer\n"
+    	                  "@vtable.BaseInterface2 = constant [1 x ptr] zeroinitializer\n"
+    	                  "@vtable.TestInterface = constant [1 x ptr] zeroinitializer\n"
+    	                  "@vtable.TestClass = constant [4 x ptr] [ptr null, ptr @TestClass_F2do, ptr @TestClass_F4undo, ptr @TestClass_F9TestClass]\n"
+    	                  "@vtable.TestClass.TestInterface = constant [1 x ptr] [ptr inttoptr (i64 -8 to ptr)]\n"
+    	                  "\n"
+    	                  "define ptr @TestClass.init_ctor(ptr %0) {\n"
+    	                  "entry:\n"
+    	                  "  %1 = alloca ptr, align 8\n"
+    	                  "  store ptr %0, ptr %1, align 8\n"
+    	                  "  %2 = load ptr, ptr %1, align 8\n"
+    	                  "  %3 = getelementptr inbounds %TestClass, ptr %2, i32 0, i32 0\n"
+    	                  "  store ptr @vtable.TestClass, ptr %3, align 8\n"
+    	                  "  %4 = getelementptr inbounds %TestClass, ptr %2, i32 0, i32 1\n"
+    	                  "  %5 = getelementptr inbounds nuw %TestInterface, ptr %4, i32 0, i32 0\n"
+    	                  "  store ptr @vtable.TestClass.TestInterface, ptr %5, align 8\n"
+    	                  "  ret ptr %2\n"
+    	                  "}\n"
+    	                  "\n"
+    	                  "define void @TestClass_F2do(ptr %0, ptr %1) {\n"
+    	                  "entry:\n"
+    	                  "  %2 = alloca ptr, align 8\n"
+    	                  "  %3 = alloca ptr, align 8\n"
+    	                  "  store ptr %0, ptr %2, align 8\n"
+    	                  "  store ptr %1, ptr %3, align 8\n"
+    	                  "  ret void\n"
+    	                  "}\n"
+    	                  "\n"
+    	                  "define void @TestClass_F4undo(ptr %0, ptr %1) {\n"
+    	                  "entry:\n"
+    	                  "  %2 = alloca ptr, align 8\n"
+    	                  "  %3 = alloca ptr, align 8\n"
+    	                  "  store ptr %0, ptr %2, align 8\n"
+    	                  "  store ptr %1, ptr %3, align 8\n"
+    	                  "  ret void\n"
+    	                  "}\n"
+    	                  "\n"
+    	                  "define void @TestClass_F9TestClass(ptr %0, ptr %1) {\n"
+    	                  "entry:\n"
+    	                  "  %2 = alloca ptr, align 8\n"
+    	                  "  %3 = alloca ptr, align 8\n"
+    	                  "  store ptr %0, ptr %2, align 8\n"
+    	                  "  store ptr %1, ptr %3, align 8\n"
+    	                  "  ret void\n"
+    	                  "}\n");
     }
 
 } // anonymous namespace
